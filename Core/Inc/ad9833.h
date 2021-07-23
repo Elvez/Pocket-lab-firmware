82d9ba8e015d507498dbca216eab6d0ee6f89e70
Elvez brachonzei <elvez.brachonzei@gmail.com>
2021-07-23 10:46:11 +0530

    AD9833 driver files added

---
+++ Core/Inc/ad9833.h
@@ -0,0 +1,15 @@
+/*
+ * ad9833.h
+ *
+ *  Created on: 22-Jul-2021
+ *      Author: elvez
+ */
+
+#ifndef INC_AD9833_H_
+#define INC_AD9833_H_
+#include <math.h>
+#include "stm32f4xx_hal.h"
+#include "com.h"
+
+
+#endif /* INC_AD9833_H_ */
---
+++ Core/Src/ad9833.c
@@ -0,0 +1,12 @@
+/*
+ * ad9833.c
+ *
+ *  Created on: 22-Jul-2021
+ *      Author: elvez
+ */
+#include "ad9833.h"
+
+
+
+
+
